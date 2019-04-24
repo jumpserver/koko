4fdac63bc23c377725447c636030115ec1d13017
ibuler <ibuler@qq.com>
2019-04-24 10:34:23 +0800

    [Update] 暂时解决冲突

--- pkg/sdk/client.go
+++ pkg/sdk/client.go
@@ -43,24 +43,14 @@ func (c *WrapperClient) CheckAuth() error {
 }
 
 func (c *WrapperClient) Get(url string, res interface{}, needAuth bool) error {
-<<<<<<< HEAD
-	//if needAuth {
-	//	c.Http.SetAuth(c.Auth.Sign())
-	//} else {
-	//	c.Http.SetAuth("")
-	//}
-=======
 	if needAuth {
 		return c.AuthClient.Get(c.BaseHost+url, res)
 	} else {
 		return c.Http.Get(c.BaseHost+url, res)
 	}
->>>>>>> 228056688660034ec5b8061e99c187aa0b1c85e7
-
 }
 
 func (c *WrapperClient) Post(url string, data interface{}, res interface{}, needAuth bool) error {
-<<<<<<< HEAD
 	//if needAuth {
 	//	c.Http.SetAuth(c.Auth.Sign())
 	//} else {
@@ -94,35 +84,4 @@ func (c *WrapperClient) Patch(url string, data interface{}, res interface{}, nee
 	//	c.Http.SetAuth("")
 	//}
 	return c.Http.Patch(url, data, res)
-=======
-	if needAuth {
-		return c.AuthClient.Post(url, data, res)
-	} else {
-		return c.Http.Post(url, data, res)
-	}
-}
-
-func (c *WrapperClient) Delete(url string, res interface{}, needAuth bool) error {
-	if needAuth {
-		return c.AuthClient.Delete(url, res)
-	} else {
-		return c.Http.Delete(url, res)
-	}
-}
-
-func (c *WrapperClient) Put(url string, data interface{}, res interface{}, needAuth bool) error {
-	if needAuth {
-		return c.AuthClient.Put(url, data, res)
-	} else {
-		return c.Http.Put(url, data, res)
-	}
-}
-
-func (c *WrapperClient) Patch(url string, data interface{}, res interface{}, needAuth bool) error {
-	if needAuth {
-		return c.AuthClient.Patch(url, data, res)
-	} else {
-		return c.Http.Patch(url, data, res)
-	}
->>>>>>> 228056688660034ec5b8061e99c187aa0b1c85e7
 }
