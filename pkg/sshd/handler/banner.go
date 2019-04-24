43e6c50b7a169fb93fba3fdc10d6cd9fb5243fa8
ibuler <ibuler@qq.com>
2019-04-24 11:13:28 +0800

    [Update] 优化结构

--- pkg/sshd/handler/banner.go
+++ pkg/sshd/handler/banner.go
@@ -40,15 +40,15 @@ func (mi *MenuItem) Text() string {
 type Menu []MenuItem
 
 var menu = Menu{
-	{instruct: "ID", helpText: "directly login or enter."},
-	{instruct: "part IP, Hostname, Comment", helpText: "to search login if unique."},
-	{instruct: "/ + IP, Hostname, Comment", helpText: "to search, such as: /192.168"},
-	{instruct: "p", helpText: "display the host you have permission."},
-	{instruct: "g", helpText: "display the node that you have permission."},
-	{instruct: "r", helpText: "refresh your assets and nodes"},
-	{instruct: "s", helpText: "switch Chinese-english language."},
-	{instruct: "h", helpText: "print help"},
-	{instruct: "q", helpText: "exit"},
+	{id: 1, instruct: "ID", helpText: "directly login or enter"},
+	{id: 2, instruct: "part IP, Hostname, Comment", helpText: "to search login if unique"},
+	{id: 3, instruct: "/ + IP, Hostname, Comment", helpText: "to search, such as: /192.168"},
+	{id: 4, instruct: "p", helpText: "display the host you have permission"},
+	{id: 5, instruct: "g", helpText: "display the node that you have permission"},
+	{id: 6, instruct: "r", helpText: "refresh your assets and nodes"},
+	{id: 7, instruct: "s", helpText: "switch Chinese-english language"},
+	{id: 8, instruct: "h", helpText: "print help"},
+	{id: 9, instruct: "q", helpText: "exit"},
 }
 
 type ColorMeta struct {
@@ -61,18 +61,12 @@ func displayBanner(sess ssh.Session, user string) {
 	if config.Conf.HeaderTitle != "" {
 		title = config.Conf.HeaderTitle
 	}
-	welcomeMsg := user + "  " + title
+	welcomeMsg := CharClear + CharTab + user + "  " + title + CharNewLine
 	_, err := io.WriteString(sess, welcomeMsg)
 	if err != nil {
 		logger.Error("Send to client error, %s", err)
 	}
-	cm := ColorMeta{GreenBoldColor: "\033[1;32m", ColorEnd: "\033[0m"}
-	for i, v := range menu {
-		line := fmt.Sprintf("\t%d) Enter {{.GreenBoldColor}}%s{{.ColorEnd}} to %s.\r\n", i+1, v.instruct, v.helpText)
-		tmpl := template.Must(template.New("item").Parse(line))
-		err := tmpl.Execute(sess, cm)
-		if err != nil {
-			logger.Error("Send to client error, %s", err)
-		}
+	for _, v := range menu {
+		_, _ = io.WriteString(sess, v.Text())
 	}
 }
