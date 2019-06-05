92ff0002890140e370bdcbdf26730d7a5a288be0
Eric <xplzv@126.com>
2019-06-05 10:28:44 +0800

    [update] add AuthDecorator

--- pkg/httpd/elfhandler.go
+++ pkg/httpd/elfhandler.go
@@ -1,14 +1,54 @@
 package httpd
 
 import (
+	"context"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/LeeEirc/elfinder"
 	socketio "github.com/googollee/go-socket.io"
 	"github.com/gorilla/mux"
+
+	"cocogo/pkg/cctx"
+	"cocogo/pkg/logger"
+	"cocogo/pkg/model"
+	"cocogo/pkg/service"
 )
+
+func AuthDecorator(handler http.HandlerFunc) http.HandlerFunc {
+	return func(responseWriter http.ResponseWriter, request *http.Request) {
+		cookies := strings.Split(request.Header.Get("Cookie"), ";")
+		var csrfToken string
+		var sessionid string
+		var remoteIP string
+		for _, line := range cookies {
+			if strings.Contains(line, "csrftoken") {
+				csrfToken = strings.Split(line, "=")[1]
+			}
+			if strings.Contains(line, "sessionid") {
+				sessionid = strings.Split(line, "=")[1]
+			}
+		}
+		user, err := service.CheckUserCookie(sessionid, csrfToken)
+		if err != nil {
+			loginUrl := fmt.Sprintf("/users/login/?next=%s", request.URL.Path)
+			http.Redirect(responseWriter, request, loginUrl, http.StatusFound)
+			return
+		}
+		xForwardFors := strings.Split(request.Header.Get("X-Forwarded-For"), ",")
+		if len(xForwardFors) >= 1 {
+			remoteIP = xForwardFors[0]
+		} else {
+			remoteIP = strings.Split(request.RemoteAddr, ":")[0]
+		}
+		ctx := context.WithValue(request.Context(), cctx.ContextKeyUser, user)
+		ctx = context.WithValue(ctx, cctx.ContextKeyRemoteAddr, remoteIP)
+		handler(responseWriter, request.WithContext(ctx))
+	}
+}
 
 func OnElfinderConnect(s socketio.Conn) error {
 	u := s.URL()
@@ -38,6 +78,9 @@ func sftpFinder(wr http.ResponseWriter, req *http.Request) {
 }
 
 func sftpHostConnectorView(wr http.ResponseWriter, req *http.Request) {
+	user := req.Context().Value(cctx.ContextKeyUser).(*model.User)
+	remoteIP := req.Context().Value(cctx.ContextKeyRemoteAddr).(string)
+	logger.Debugf("user: %s; remote ip: %s; create connector", user.Name, remoteIP)
 	con := elfinder.NewElFinderConnector([]elfinder.Volume{&elfinder.DefaultVolume})
 	con.ServeHTTP(wr, req)
 }
