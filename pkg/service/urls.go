ebd7b1c230e321f91069fb60fb7127ec162513d5
ibuler <ibuler@qq.com>
2019-08-30 11:18:31 +0800

    [Update] 修改url

--- pkg/service/urls.go
+++ pkg/service/urls.go
@@ -1,36 +1,36 @@
 package service
 
 const (
-	UserAuthURL    = "/api/authentication/v1/auth/"                      // post 验证用户登陆
-	UserProfileURL = "/api/users/v1/profile/"                            // 获取当前用户的基本信息
-	UserListURL    = "/api/users/v1/users/"                              // 用户列表地址
-	UserDetailURL  = "/api/users/v1/users/%s/"                           // 获取用户信息
-	UserAuthOTPURL = "/api/authentication/v1/otp/auth/"                  // 验证OTP
-	TokenAssetURL  = "/api/authentication/v1/connection-token/?token=%s" // Token name
+	UserAuthURL    = "/api/v1/authentication/auth/"                      // post 验证用户登陆
+	UserProfileURL = "/api/v1/users/profile/"                            // 获取当前用户的基本信息
+	UserListURL    = "/api/v1/users/users/"                              // 用户列表地址
+	UserDetailURL  = "/api/v1/users/users/%s/"                           // 获取用户信息
+	UserAuthOTPURL = "/api/v1/authentication/otp/auth/"                  // 验证OTP
+	TokenAssetURL  = "/api/v1/authentication/connection-token/?token=%s" // Token name
 
-	SystemUserAssetAuthURL          = "/api/assets/v1/system-user/%s/asset/%s/auth-info/" // 该系统用户对某资产的授权
-	SystemUserCmdFilterRulesListURL = "/api/assets/v1/system-user/%s/cmd-filter-rules/"   // 过滤规则url
-	SystemUserDetailURL             = "/api/assets/v1/system-user/%s/"                    // 某个系统用户的信息
-	AssetDetailURL                  = "/api/assets/v1/assets/%s/"                         // 某一个资产信息
-	DomainDetailURL                 = "/api/assets/v1/domain/%s/?gateway=1"
+	SystemUserAssetAuthURL          = "/api/v1/assets/system-users/%s/assets/%s/auth-info/" // 该系统用户对某资产的授权
+	SystemUserCmdFilterRulesListURL = "/api/v1/assets/system-users/%s/cmd-filter-rules/"    // 过滤规则url
+	SystemUserDetailURL             = "/api/v1/assets/system-users/%s/"                     // 某个系统用户的信息
+	AssetDetailURL                  = "/api/v1/assets/assets/%s/"                           // 某一个资产信息
+	DomainDetailURL                 = "/api/v1/assets/domains/%s/?gateway=1"
 
-	TerminalRegisterURL  = "/api/terminal/v2/terminal-registrations/" // 注册当前coco
-	TerminalConfigURL    = "/api/terminal/v1/terminal/config/"        // 从jumpserver获取coco的配置
-	TerminalHeartBeatURL = "/api/terminal/v1/terminal/status/"
+	TerminalRegisterURL  = "/api/v2/terminal/terminal-registrations/" // 注册当前coco
+	TerminalConfigURL    = "/api/v1/terminal/terminals/config/"       // 从jumpserver获取coco的配置
+	TerminalHeartBeatURL = "/api/v1/terminal/terminals/status/"
 
-	SessionListURL    = "/api/terminal/v1/sessions/"           //上传创建的资产会话session id
-	SessionDetailURL  = "/api/terminal/v1/sessions/%s/"        // finish session的时候发送
-	SessionReplayURL  = "/api/terminal/v1/sessions/%s/replay/" //上传录像
-	SessionCommandURL = "/api/terminal/v1/command/"            //上传批量命令
-	FinishTaskURL     = "/api/terminal/v1/tasks/%s/"
+	SessionListURL    = "/api/v1/terminal/sessions/"           //上传创建的资产会话session id
+	SessionDetailURL  = "/api/v1/terminal/sessions/%s/"        // finish session的时候发送
+	SessionReplayURL  = "/api/v1/terminal/sessions/%s/replay/" //上传录像
+	SessionCommandURL = "/api/v1/terminal/commands/"           //上传批量命令
+	FinishTaskURL     = "/api/v1/terminal/tasks/%s/"
 
-	FTPLogListURL = "/api/audits/v1/ftp-log/" // 上传 ftp日志
+	FTPLogListURL = "/api/v1/audits/ftp-logs/" // 上传 ftp日志
 
-	UserAssetsURL                  = "/api/perms/v1/users/%s/assets/"       //获取用户授权的所有资产
-	UserNodesAssetsURL             = "/api/perms/v1/users/%s/nodes-assets/" // 获取用户授权的所有节点信息 节点分组
-	UserNodesListURL               = "/api/perms/v1/users/%s/nodes/"
-	UserNodeAssetsListURL          = "/api/perms/v1/users/%s/nodes/%s/assets/"
-	ValidateUserAssetPermissionURL = "/api/perms/v1/asset-permissions/user/validate/" //0不使用缓存 1 使用缓存 2 刷新缓存
+	UserAssetsURL                  = "/api/v1/perms/users/%s/assets/"       //获取用户授权的所有资产
+	UserNodesAssetsURL             = "/api/v1/perms/users/%s/nodes-assets/" // 获取用户授权的所有节点信息 节点分组
+	UserNodesListURL               = "/api/v1/perms/users/%s/nodes/"
+	UserNodeAssetsListURL          = "/api/v1/perms/users/%s/nodes/%s/assets/"
+	ValidateUserAssetPermissionURL = "/api/v1/perms/asset-permissions/user/validate/" //0不使用缓存 1 使用缓存 2 刷新缓存
 )
 
 // 1.5.3
