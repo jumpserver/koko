aad186392f54975c9889c50c5a8895eccbf272d0
Eric <xplzv@126.com>
2019-10-09 15:30:33 +0800

    [bugfix] cat二进制文件 界面卡顿

--- pkg/proxy/parsercmd.go
+++ pkg/proxy/parsercmd.go
@@ -19,56 +19,70 @@ func NewCmdParser() *CmdParser {
 }
 
 type CmdParser struct {
-	term *utils.Terminal
-	reader io.ReadCloser
-	writer io.WriteCloser
-	currentLines []string
-	lock *sync.Mutex
-	maxLength int
+	term          *utils.Terminal
+	reader        io.ReadCloser
+	writer        io.WriteCloser
+	currentLines  []string
+	lock          *sync.Mutex
+	maxLength     int
 	currentLength int
-
+	closed        chan struct{}
 }
 
-func (cp *CmdParser) WriteData(p []byte) (int,error){
+func (cp *CmdParser) WriteData(p []byte) (int, error) {
 	return cp.writer.Write(p)
 }
 
-func (cp *CmdParser) Write (p []byte) (int,error){
-	return len(p),nil
+func (cp *CmdParser) Write(p []byte) (int, error) {
+	return len(p), nil
 }
 
-func (cp *CmdParser) Read(p []byte)(int,error){
+func (cp *CmdParser) Read(p []byte) (int, error) {
 	return cp.reader.Read(p)
 }
 
-func (cp *CmdParser) Close() error{
+func (cp *CmdParser) Close() error {
+	select {
+	case <-cp.closed:
+		return nil
+	default:
+		close(cp.closed)
+	}
 	return cp.writer.Close()
 }
 
 func (cp *CmdParser) initial() {
-	cp.reader,cp.writer = io.Pipe()
-	cp.currentLines = make([]string,0)
+	cp.reader, cp.writer = io.Pipe()
+	cp.currentLines = make([]string, 0)
 	cp.lock = new(sync.Mutex)
 	cp.maxLength = 1024
 	cp.currentLength = 0
+	cp.closed = make(chan struct{})
 
 	cp.term = utils.NewTerminal(cp, "")
 	cp.term.SetEcho(false)
 	go func() {
 		logger.Debug("command Parser start")
 		defer logger.Debug("command Parser close")
+	inloop:
 		for {
 			line, err := cp.term.ReadLine()
-			if err != nil{
-				break
+			if err != nil {
+				select {
+				case <-cp.closed:
+					goto outloop
+				default:
+				}
+				goto inloop
 			}
 			cp.lock.Lock()
 			cp.currentLength += len(line)
 			if cp.currentLength < cp.maxLength {
-				cp.currentLines = append(cp.currentLines,line)
+				cp.currentLines = append(cp.currentLines, line)
 			}
 			cp.lock.Unlock()
 		}
+	outloop:
 	}()
 }
 
@@ -83,7 +97,7 @@ func (cp *CmdParser) Parse() string {
 	defer cp.lock.Unlock()
 	output := strings.TrimSpace(strings.Join(cp.currentLines, "\r\n"))
 	output = cp.parsePS1(output)
-	cp.currentLines = make([]string,0)
+	cp.currentLines = make([]string, 0)
 	cp.currentLength = 0
 	return output
 }
