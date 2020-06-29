69e217b8e9f154d82dccedc0a0ad38ef09cad9e9
ibuler <ibuler@qq.com>
2020-06-29 14:00:54 +0800

    fix(entrypoint): 修改入口脚本

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
-
-# 可以设置一些ENV
+#!/bin/bash -eux
 
 # 输出所有内容到目录releases中
+build_stamp=$(date -u '+%Y-%m-%d %I:%M:%S%p')
+git_hash=$(git rev-parse HEAD || echo 'None')
+go_version=$(go version)
 
-cd cmd && go build -ldflags "-X 'main.Buildstamp=`date -u '+%Y-%m-%d %I:%M:%S%p'`' -X 'main.Githash=`git rev-parse HEAD`' -X 'main.Goversion=`go version`'" -x -o koko koko.go
+cd cmd && go build -ldflags "-X 'main.Buildstamp=${build_stamp}' -X 'main.Githash=${git_hash}' -X 'main.Goversion=${go_version}'" -o koko koko.go
 
 exit_code=$?
 if [ ${exit_code} != 0 ];then
@@ -13,5 +14,11 @@ if [ ${exit_code} != 0 ];then
 fi
 
 mkdir -p ../release && rm -rf ../release/*
-mkdir -p ../release/koko
-cp -R koko locale static templates config_example.yml ../release/koko
+output_dir="../release/koko"
+mkdir -p ${output_dir}
+cp -R koko locale static templates config_example.yml ${output_dir}
+
+output=$(cd ${output_dir} && pwd)
+echo "${output}"
+
+
