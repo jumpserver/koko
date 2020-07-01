01de91d302132f4ab75587d88461cf155a558975
Eric <xplzv@126.com>
2020-07-01 14:00:46 +0800

    remove gb18030 gb2312 big5

--- pkg/model/platform.go
+++ pkg/model/platform.go
@@ -2,16 +2,12 @@ package model
 
 import (
 	"golang.org/x/text/encoding/simplifiedchinese"
-	"golang.org/x/text/encoding/traditionalchinese"
 	"golang.org/x/text/transform"
 )
 
 const (
 	UTF8 = "utf8"
 	GBK = "gbk"
-	GB18030 = "gb18030"
-	GB2312 = "gb2312"
-	BIG5 = "big5"
 )
 
 type Platform struct {
@@ -26,12 +22,6 @@ func LookupCharsetDecode(charset string) transform.Transformer{
 	switch charset {
 	case GBK:
 		return simplifiedchinese.GBK.NewDecoder()
-	case GB18030:
-		return simplifiedchinese.GB18030.NewDecoder()
-	case GB2312:
-		return simplifiedchinese.HZGB2312.NewDecoder()
-	case BIG5:
-		return traditionalchinese.Big5.NewDecoder()
 	}
 	return nil
 }
@@ -39,12 +29,6 @@ func LookupCharsetEncode(charset string) transform.Transformer{
 	switch charset {
 	case GBK:
 		return simplifiedchinese.GBK.NewEncoder()
-	case GB18030:
-		return simplifiedchinese.GB18030.NewEncoder()
-	case GB2312:
-		return simplifiedchinese.HZGB2312.NewEncoder()
-	case BIG5:
-		return traditionalchinese.Big5.NewEncoder()
 	}
 	return nil
 }
