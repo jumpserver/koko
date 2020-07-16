8539a910a8f0a6e63cb31d11d9393b6dc774562b
Eric <xplzv@126.com>
2020-07-16 15:22:13 +0800

    fix: mysql default charset utf8

--- pkg/srvconn/mysqlconn.go
+++ pkg/srvconn/mysqlconn.go
@@ -34,7 +34,7 @@ mount -t tmpfs -o size=10M tmpfs /nonexistent
 cd /nonexistent
 export HOME=/nonexistent
 export TMPDIR=/nonexistent
-exec su -s /bin/bash --command="mysql --default-character-set=utf-8 --user=${USERNAME} --host=${HOSTNAME} --port=${PORT} --password ${DATABASE}" nobody
+exec su -s /bin/bash --command="mysql --default-character-set=utf8 --user=${USERNAME} --host=${HOSTNAME} --port=${PORT} --password ${DATABASE}" nobody
 `
 
 var mysqlOnce sync.Once
@@ -249,7 +249,7 @@ type SqlOptions struct {
 
 func (opts *SqlOptions) CommandArgs() []string {
 	return []string{
-		"--default-character-set=utf-8",
+		"--default-character-set=utf8",
 		fmt.Sprintf("--user=%s", opts.Username),
 		fmt.Sprintf("--host=%s", opts.Host),
 		fmt.Sprintf("--port=%d", opts.Port),
