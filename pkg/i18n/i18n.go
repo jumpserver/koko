c3845b3f73111d76be904b655e9c378b95de98bf
Eric <xplzv@126.com>
2020-08-12 17:04:33 +0800

    fix(language): default zh language code

--- pkg/i18n/i18n.go
+++ pkg/i18n/i18n.go
@@ -12,10 +12,10 @@ import (
 func Initial()() {
 	cf := config.GetConf()
 	localePath := path.Join(cf.RootPath, "locale")
-	if strings.HasPrefix(cf.Language, "zh") {
-		gotext.Configure(localePath, "zh_CN", "koko")
-	} else {
+	if strings.HasPrefix(strings.ToLower(cf.LanguageCode), "en") {
 		gotext.Configure(localePath, "en_US", "koko")
+	} else {
+		gotext.Configure(localePath, "zh_CN", "koko")
 	}
 }
 
