e71bea5212a0345a77e725937cfb6091da5ab748
wooos <819844849@qq.com>
2021-03-16 00:00:19 +0800

    feat: 自动补全功能

--- pkg/utils/util.go
+++ pkg/utils/util.go
@@ -51,3 +51,51 @@ func IgnoreErrWriteWindowTitle(writer io.Writer, title string) {
 	// Ps = 2  ⇒  Change Window Title to Pt.
 	_, _ = writer.Write([]byte(fmt.Sprintf("\x1b]2;%s\x07", title)))
 }
+
+func LongestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+
+	isCommonPrefix := func(length int) bool {
+		str0, count := strs[0][:length], len(strs)
+		for i := 1; i < count; i++ {
+			if strs[i][:length] != str0 {
+				return false
+			}
+		}
+		return true
+	}
+
+	minLength := len(strs[0])
+	for _, s := range strs {
+		if len(s) < minLength {
+			minLength = len(s)
+		}
+
+	}
+
+	low, high := 0, minLength
+	for low < high {
+		mid := (high-low+1)/2 + low
+		if isCommonPrefix(mid) {
+			low = mid
+		} else {
+			high = mid - 1
+		}
+
+	}
+	return strs[0][:low]
+}
+
+func FilterPrefix(strs []string, s string) (r []string) {
+	for _, v := range strs {
+		if len(v) >= len(s) {
+			if v[:len(s)] == s {
+				r = append(r, v)
+			}
+		}
+	}
+
+	return r
+}
