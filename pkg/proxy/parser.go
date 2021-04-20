f600071b06f17b50d30dd5d7c149036d6c832890
Eric <xplzv@126.com>
2021-04-20 17:57:15 +0800

    feat: 命令复核

--- pkg/proxy/parser.go
+++ pkg/proxy/parser.go
@@ -2,15 +2,19 @@ package proxy
 
 import (
 	"bytes"
+	"context"
 	"fmt"
+	"sort"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/LeeEirc/tclientlib"
 
 	"github.com/jumpserver/koko/pkg/i18n"
 	"github.com/jumpserver/koko/pkg/logger"
 	"github.com/jumpserver/koko/pkg/model"
+	"github.com/jumpserver/koko/pkg/service"
 	"github.com/jumpserver/koko/pkg/utils"
 )
 
@@ -53,6 +57,7 @@ func newParser(sid, protocolType string) Parser {
 }
 
 // Parse 解析用户输入输出, 拦截过滤用户输入输出
+
 type Parser struct {
 	id           string
 	protocolType string
@@ -76,6 +81,8 @@ type Parser struct {
 
 	cmdFilterRules []model.SystemUserFilterRule
 	closed         chan struct{}
+
+	confirmStatus commandConfirmStatus
 }
 
 func (p *Parser) initial() {
@@ -143,14 +150,58 @@ func (p *Parser) ParseStream(userInChan chan *model.RoomMessage, srvInChan <-cha
 	return p.userOutputChan, p.srvOutputChan
 }
 
-// Todo: parseMultipleInput 依然存在问题
-
 // parseInputState 切换用户输入状态, 并结算命令和结果
 func (p *Parser) parseInputState(b []byte) []byte {
+	p.lock.Lock()
 	if p.inVimState || p.zmodemState != "" {
 		return b
 	}
 	p.inputPreState = p.inputState
+	p.lock.Unlock()
+
+	if p.confirmStatus.InRunning() {
+		if p.confirmStatus.IsNeedCancel(b) {
+			logger.Infof("Session %s: user cancel confirm status", p.id)
+			p.srvOutputChan <- []byte("\r\n")
+			return breakInputPacket(p.protocolType)
+		}
+		logger.Infof("Session %s: command confirm status %s, drop input", p.id,
+			p.confirmStatus.Status)
+		return nil
+	}
+	waitMsg := i18n.T("the reviewers will confirm. continue or not [Y/n]")
+	if p.confirmStatus.InQuery() {
+		switch strings.ToLower(string(b)) {
+		case "y":
+			p.confirmStatus.SetStatus(StatusStart)
+			p.confirmStatus.wg.Add(1)
+			go func() {
+				p.confirmStatus.SetAction(model.ActionUnknown)
+				p.waitCommandConfirm()
+				switch p.confirmStatus.GetAction() {
+				case model.ActionAllow:
+					p.srvOutputChan <- []byte("\r\n")
+					p.userOutputChan <- []byte(p.confirmStatus.data)
+				case model.ActionDeny:
+					p.forbiddenCommand(p.confirmStatus.Cmd)
+				default:
+					// 默认是取消 不执行
+					p.srvOutputChan <- []byte("\r\n")
+					p.userOutputChan <- breakInputPacket(p.protocolType)
+				}
+				// 审核结束, 重置状态
+				p.confirmStatus.SetStatus(StatusNone)
+				p.confirmStatus.wg.Done()
+			}()
+		case "n":
+			p.confirmStatus.SetStatus(StatusNone)
+			p.srvOutputChan <- []byte("\r\n")
+			return breakInputPacket(p.protocolType)
+		default:
+			p.srvOutputChan <- []byte("\r\n" + waitMsg)
+		}
+		return nil
+	}
 
 	if bytes.Contains(b, charEnter) {
 		// 连续输入enter key, 结算上一条可能存在的命令结果
@@ -158,13 +209,21 @@ func (p *Parser) parseInputState(b []byte) []byte {
 		p.inputState = false
 		// 用户输入了Enter，开始结算命令
 		p.parseCmdInput()
-		if cmd, ok := p.IsCommandForbidden(); !ok {
-			fbdMsg := utils.WrapperWarn(fmt.Sprintf(i18n.T("Command `%s` is forbidden"), cmd))
-			p.srvOutputChan <- []byte("\r\n" + fbdMsg)
-			p.cmdRecordChan <- [3]string{p.command, fbdMsg, model.HighRiskFlag}
-			p.command = ""
-			p.output = ""
-			return breakInputPacket(p.protocolType)
+		if rule, cmd, ok := p.IsMatchCommandRule(p.command); ok {
+			switch rule.Action {
+			case model.ActionDeny:
+				p.forbiddenCommand(cmd)
+				return nil
+			case model.ActionConfirm:
+				p.confirmStatus.SetStatus(StatusQuery)
+				p.confirmStatus.SetRule(rule)
+				p.confirmStatus.SetCmd(cmd)
+				p.confirmStatus.SetData(string(b))
+				p.confirmStatus.ResetCtx()
+				p.srvOutputChan <- []byte("\r\n" + waitMsg)
+				return nil
+			default:
+			}
 		}
 	} else {
 		p.inputState = true
@@ -177,6 +236,15 @@ func (p *Parser) parseInputState(b []byte) []byte {
 		}
 	}
 	return b
+}
+
+func (p *Parser) forbiddenCommand(cmd string) {
+	fbdMsg := utils.WrapperWarn(fmt.Sprintf(i18n.T("Command `%s` is forbidden"), cmd))
+	p.srvOutputChan <- []byte("\r\n" + fbdMsg)
+	p.cmdRecordChan <- [3]string{p.command, fbdMsg, model.HighRiskFlag}
+	p.command = ""
+	p.output = ""
+	p.userOutputChan <- breakInputPacket(p.protocolType)
 }
 
 // parseCmdInput 解析命令的输入
@@ -196,8 +264,6 @@ func (p *Parser) parseCmdOutput() {
 
 // ParseUserInput 解析用户的输入
 func (p *Parser) ParseUserInput(b []byte) []byte {
-	p.lock.Lock()
-	defer p.lock.Unlock()
 	p.once.Do(func() {
 		p.inputInitial = true
 	})
@@ -266,24 +332,118 @@ func (p *Parser) ParseServerOutput(b []byte) []byte {
 
 // SetCMDFilterRules 设置命令过滤规则
 func (p *Parser) SetCMDFilterRules(rules []model.SystemUserFilterRule) {
+	// 优先级排序
+	sort.Sort(model.FilterRules(rules))
 	p.cmdFilterRules = rules
 }
 
-// IsCommandForbidden 判断命令是不是在过滤规则中
-func (p *Parser) IsCommandForbidden() (string, bool) {
+// IsMatchCommandRule 判断命令是不是在过滤规则中
+func (p *Parser) IsMatchCommandRule(command string) (model.SystemUserFilterRule, string, bool) {
 	for _, rule := range p.cmdFilterRules {
-		allowed, cmd := rule.Match(p.command)
+		allowed, cmd := rule.Match(command)
 		switch allowed {
 		case model.ActionAllow:
-			return "", true
-		case model.ActionDeny:
-			return cmd, false
+			return model.SystemUserFilterRule{}, cmd, true
+		case model.ActionConfirm, model.ActionDeny:
+			return rule, cmd, true
 		default:
-
 		}
 	}
-	return "", true
+	return model.SystemUserFilterRule{}, "", false
 }
+
+func (p *Parser) waitCommandConfirm() {
+	cmd := p.confirmStatus.Cmd
+	resp, err := service.SubmitCommandConfirm(p.id, p.confirmStatus.Rule.ID, p.confirmStatus.Cmd)
+	if err != nil {
+		logger.Errorf("Session %s: submit command confirm api err: %s", p.id, err)
+		p.confirmStatus.SetAction(model.ActionDeny)
+		return
+	}
+	checkReq := resp.CheckConfirmStatus
+	cancelReq := resp.CloseConfirm
+	detailURL := resp.TicketDetailUrl
+	reviewers := resp.Reviewers
+	msg := i18n.T("Please waiting for the reviewers to confirm command `%s`, cancel by CTRL+C.")
+	waitMsg := fmt.Sprintf(msg, cmd)
+	checkTimer := time.NewTicker(10 * time.Second)
+	defer checkTimer.Stop()
+	ctx, cancelFunc := context.WithCancel(p.confirmStatus.ctx)
+	defer cancelFunc()
+	go func() {
+		delay := 0
+		titleMsg := i18n.T("Need ticket confirm to execute command, already send email to the reviewers")
+		reviewersMsg := fmt.Sprintf(i18n.T("Ticket Reviewers: %s"), strings.Join(reviewers, ", "))
+		detailURLMsg := fmt.Sprintf(i18n.T("Could copy website URL to notify reviewers: %s"), detailURL)
+		var tipString strings.Builder
+		tipString.WriteString(utils.CharNewLine)
+		tipString.WriteString(titleMsg)
+		tipString.WriteString(utils.CharNewLine)
+		tipString.WriteString(reviewersMsg)
+		tipString.WriteString(utils.CharNewLine)
+		tipString.WriteString(detailURLMsg)
+		tipString.WriteString(utils.CharNewLine)
+		p.srvOutputChan <- []byte(utils.WrapperString(tipString.String(), utils.Green))
+		for {
+			select {
+			case <-p.closed:
+				return
+			case <-ctx.Done():
+				return
+			default:
+				delayS := fmt.Sprintf("%ds", delay)
+				data := strings.Repeat("\x08", len(delayS)+len(waitMsg)) + waitMsg + delayS
+				p.srvOutputChan <- []byte(data)
+				time.Sleep(time.Second)
+				delay += 1
+			}
+		}
+	}()
+	for {
+		select {
+		case <-p.closed:
+			if err = service.CancelConfirmByRequestInfo(cancelReq); err != nil {
+				logger.Errorf("Session %s: Cancel command confirm err: %s", p.id, err)
+			}
+			logger.Infof("Session %s: Closed", p.id)
+			return
+		case <-ctx.Done():
+			// 取消
+			if err = service.CancelConfirmByRequestInfo(cancelReq); err != nil {
+				logger.Errorf("Session %s: Cancel command confirm err: %s", p.id, err)
+			}
+			logger.Infof("Session %s: Cancel confirm command", p.id)
+			return
+		case <-checkTimer.C:
+		}
+		statusResp, err := service.CheckConfirmStatusByRequestInfo(checkReq)
+		if err != nil {
+			logger.Errorf("Session %s: check command confirm status err: %s", p.id, err)
+			continue
+		}
+		switch statusResp.Status {
+		case approve:
+			p.confirmStatus.SetAction(model.ActionAllow)
+			p.confirmStatus.SetProcessor(statusResp.Processor)
+			return
+		case reject:
+			p.confirmStatus.SetProcessor(statusResp.Processor)
+			p.confirmStatus.SetAction(model.ActionDeny)
+			return
+		case await:
+			continue
+		default:
+			logger.Errorf("Receive unknown command confirm status %s",
+				statusResp.Status)
+		}
+	}
+}
+
+const (
+	approve = "approve"
+	reject  = "reject"
+	await   = "await"
+)
 
 func (p *Parser) IsInZmodemRecvState() bool {
 	p.lock.Lock()
