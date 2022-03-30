17b90cdd0650c9f7f9d180ac9f98bb00d7d8eeda
Eric <xplzv@126.com>
2022-03-30 17:41:58 +0800

    fix: 修复 k8s 显示 token 问题

--- cmd/kubectl/kubectl.go
+++ cmd/kubectl/kubectl.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
+	"log"
 	"os"
 	"os/exec"
 	"os/signal"
@@ -47,6 +50,60 @@ func main() {
 
 	commandString := fmt.Sprintf("%s %s", commandPrefix, s.String())
 	c := exec.Command("bash", "-c", commandString)
-	c.Stdin, c.Stdout, c.Stderr = os.Stdin, os.Stdout, os.Stderr
+	c.Stdin, c.Stdout = os.Stdin, os.Stdout
+	stderr, err := c.StderrPipe()
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+	redirectStream := func() {
+		_, _ = io.Copy(os.Stderr, stderr)
+	}
+	if token != "" {
+		redirectStream = func() {
+			hiddenTokenOutput(stderr, os.Stderr, token)
+		}
+	}
+	go redirectStream()
 	_ = c.Run()
+}
+
+func hiddenTokenOutput(src io.ReadCloser, dst io.WriteCloser, token string) {
+	tokenBuf := []byte(token)
+	buf := make([]byte, 1024*8)
+	var (
+		index  int
+		remain []byte
+		buffer bytes.Buffer
+	)
+	for {
+		nr, err2 := src.Read(buf)
+		if nr > 0 {
+			for i := range buf[:nr] {
+				if index == len(tokenBuf) {
+					index = 0
+					remain = nil
+					buffer.WriteString("*****")
+					buffer.WriteByte(buf[i])
+					continue
+				}
+				if buf[i] == tokenBuf[index] {
+					index++
+					remain = append(remain, buf[i])
+					continue
+				}
+				if len(remain) > 0 {
+					buffer.Write(remain)
+					remain = nil
+				}
+				index = 0
+				buffer.WriteByte(buf[i])
+			}
+			_, _ = buffer.WriteTo(dst)
+			buffer.Reset()
+		}
+		if err2 != nil {
+			return
+		}
+	}
 }
