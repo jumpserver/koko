87ec2587e6752e767eb0695caa1f8c97c6592ed5
halo <wuyihuangw@gmail.com>
2022-07-31 23:28:41 +0800

    fix: 复制粘贴多行命令不能阻断bug

--- pkg/proxy/parser.go
+++ pkg/proxy/parser.go
@@ -317,7 +317,7 @@ func (p *Parser) parseCmdInput() {
 	if len(commands) <= 0 {
 		p.command = ""
 	} else {
-		p.command = commands[len(commands)-1]
+		p.command = strings.Join(commands, "\r\n")
 	}
 	p.cmdCreateDate = time.Now()
 }
