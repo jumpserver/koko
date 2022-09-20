e6bef8eed4557697269ea410033c2454fb7aae21
“huailei000” <2280131253@qq.com>
2022-09-20 15:12:27 +0800

    fix: 修复语言设置不准确问题

--- ui/src/i18n/langs/index.js
+++ ui/src/i18n/langs/index.js
@@ -1,12 +1,12 @@
 import zhLocale from 'element-ui/lib/locale/lang/zh-CN'
 import enLocale from 'element-ui/lib/locale/lang/en'
 import jaLocale from 'element-ui/lib/locale/lang/ja'
-import zh from './cn.json'
+import zh from './zh.json'
 import en from './en.json'
 import ja from './ja.json'
 
 export default {
-  cn: {
+  zh: {
     ...zhLocale,
     ...zh
   },
