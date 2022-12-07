ffed0e62c3fa81bc4e42e8fdff7937dabe910589
Eric <xplzv@126.com>
2022-12-07 12:46:41 +0800

    perf: command review acl ticket

--- pkg/jms-sdk-go/service/jms_test.go
+++ pkg/jms-sdk-go/service/jms_test.go
@@ -44,22 +44,6 @@ func TestJMService_GetTerminalConfig(t *testing.T) {
 	t.Logf("%+v", conf)
 }
 
-func TestJMService_GetAssetById(t *testing.T) {
-	jms := setup()
-	assetIds := []string{
-		"2e73f0e4-13ec-4f64-b03e-4ecbadab7233", // 有网域
-		"bd87e0b9-9a94-48df-9fa1-4aab4c9f49a5", // 无网域
-	}
-	for i := range assetIds {
-		asset, err := jms.GetAssetDetailById(assetIds[i])
-		if err != nil {
-			t.Fatal(err)
-		}
-		t.Logf("%+v\n", asset)
-	}
-
-}
-
 func TestJMService_GetDomainGateways(t *testing.T) {
 	jms := setup()
 	domains := []string{
@@ -72,15 +56,6 @@ func TestJMService_GetDomainGateways(t *testing.T) {
 		}
 		t.Logf("%+v\n", asset)
 	}
-}
-
-func TestJMService_SubmitCommandConfirm(t *testing.T) {
-	jms := setup()
-	sid := "8e7df6b6-795c-4904-bd17-f3bf2855ae9f"
-	ruleId := "0fd1112f-1c14-4457-bff8-62e21b1a64a2"
-	command := "ls"
-	res, err := jms.SubmitCommandConfirm(sid, ruleId, command)
-	t.Log(res, err)
 }
 
 func TestJMService_GetPublicSetting(t *testing.T) {
