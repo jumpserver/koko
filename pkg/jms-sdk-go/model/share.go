ee95f5ade9ec26a48f852a65a4533afe926e91fc
Eric <xplzv@126.com>
2023-01-03 17:39:07 +0800

    fix: sharing session

--- pkg/jms-sdk-go/model/share.go
+++ pkg/jms-sdk-go/model/share.go
@@ -4,19 +4,16 @@ type SharingSession struct {
 	ID          string `json:"id"`
 	IsActive    bool   `json:"is_active"`
 	ExpiredTime int    `json:"expired_time"`
-	Session     string `json:"session"`
-	OrgId       string `json:"org_id"`
-	OrgName     string `json:"org_name"`
 	Code        string `json:"verify_code"`
 }
 
 type ShareRecord struct {
-	ID        string `json:"id"`
-	Code      string `json:"verify_code"`
-	SessionId string `json:"session"`
-	ShareId   string `json:"sharing"`
-	OrgId     string `json:"org_id"`
-	OrgName   string `json:"org_name"`
-	Joiner    string `json:"joiner"`
+	ID        string      `json:"id"`
+	Code      string      `json:"verify_code"`
+	SessionId string      `json:"session"`
+	ShareId   string      `json:"sharing"`
+	OrgId     string      `json:"org_id"`
+	OrgName   string      `json:"org_name"`
+	Joiner    string      `json:"joiner"`
 	Err       interface{} `json:"error"`
 }
