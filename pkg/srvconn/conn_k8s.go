552e913a3a9a6e0095fcabf78d16459b3f4d3d89
Eric <xplzv@126.com>
2023-07-26 16:06:06 +0800

    perf: update k8s token

--- pkg/srvconn/conn_k8s.go
+++ pkg/srvconn/conn_k8s.go
@@ -37,9 +37,8 @@ func IsValidK8sUserToken(o *k8sOptions) bool {
 		logger.Errorf("K8sCon check token err: %s", err)
 		return false
 	}
-	spec := v1.TokenReviewSpec{Token: o.Token}
 	resp, err := client.AuthenticationV1().TokenReviews().Create(context.TODO(),
-		&v1.TokenReview{Spec: spec}, metav1.CreateOptions{})
+		&v1.TokenReview{}, metav1.CreateOptions{})
 	if err != nil {
 		logger.Errorf("K8sCon check token pods err: %s", err)
 		return false
