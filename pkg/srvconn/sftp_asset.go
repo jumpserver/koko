ba614102536815fc36b753da270de67a6ed41317
Eric <xplzv@126.com>
2023-09-13 13:07:53 +0800

    perf: 特殊处理 sftp root 账号连接的目录权限问题

--- pkg/srvconn/sftp_asset.go
+++ pkg/srvconn/sftp_asset.go
@@ -264,16 +264,16 @@ func (ad *AssetDir) ReadDir(path string) (res []os.FileInfo, err error) {
 		return nil, sftp.ErrSshFxConnectionLost
 	}
 	res, err = con.client.ReadDir(realPath)
-	if !ad.ShowHidden {
-		noHiddenFiles := make([]os.FileInfo, 0, len(res))
-		for i := 0; i < len(res); i++ {
-			if !strings.HasPrefix(res[i].Name(), ".") {
-				noHiddenFiles = append(noHiddenFiles, res[i])
-			}
+	isRootAccount := con.token.Account.Username == "root"
+	noHiddenFiles := make([]os.FileInfo, 0, len(res))
+	for i := 0; i < len(res); i++ {
+		info := NewSftpFileInfo(res[i], isRootAccount)
+		if !ad.ShowHidden && strings.HasPrefix(info.Name(), ".") {
+			continue
 		}
-		return noHiddenFiles, err
+		noHiddenFiles = append(noHiddenFiles, info)
 	}
-	return
+	return noHiddenFiles, err
 }
 
 func (ad *AssetDir) ReadLink(path string) (res string, err error) {
@@ -422,7 +422,8 @@ func (ad *AssetDir) Stat(path string) (res os.FileInfo, err error) {
 		return nil, sftp.ErrSshFxConnectionLost
 	}
 	res, err = con.client.Stat(realPath)
-	return
+	isRootAccount := con.token.Account.Username == "root"
+	return NewSftpFileInfo(res, isRootAccount), err
 }
 
 func (ad *AssetDir) Symlink(oldNamePath, newNamePath string) (err error) {
@@ -726,4 +727,47 @@ func (ad *AssetDir) CreateFTPLog(su *model.PermAccount, operate, filename string
 func IsExistPath(client *sftp.Client, path string) bool {
 	_, err := client.Stat(path)
 	return err == nil
+}
+
+func NewSftpFileInfo(info os.FileInfo, isRoot bool) os.FileInfo {
+	if !isRoot {
+		return info
+	}
+	return &SftpFileInfo{info: info, isRoot: isRoot}
+}
+
+type SftpFileInfo struct {
+	info   os.FileInfo
+	isRoot bool
+}
+
+func (ad *SftpFileInfo) Name() string {
+	return ad.info.Name()
+}
+
+func (ad *SftpFileInfo) Size() int64 {
+	return ad.info.Size()
+}
+
+/*
+	特殊处理：
+		如果是 root 账号，获取的目录信息，手动修改其文件权限可读写,
+		允许其在 web sftp 可以上传文件
+*/
+
+func (ad *SftpFileInfo) Mode() os.FileMode {
+	if ad.isRoot && ad.info.IsDir() {
+		return ad.info.Mode() | os.ModePerm
+	}
+	return ad.info.Mode()
+}
+
+func (ad *SftpFileInfo) ModTime() time.Time {
+	return ad.info.ModTime()
+}
+
+func (ad *SftpFileInfo) IsDir() bool { return ad.info.IsDir() }
+
+func (ad *SftpFileInfo) Sys() interface{} {
+	return ad.info.Sys()
 }
