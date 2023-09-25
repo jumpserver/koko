1d01bfb70648553a9f3ef7975f738247c3b6c450
Eric <xplzv@126.com>
2023-09-25 21:34:51 +0800

    fix: 限制 nobody 的执行权限
    fix: 限制命令执行权限
    
    fix: 修复部分执行权限

---
+++ pkg/srvconn/conn_nobody.go
@@ -0,0 +1,23 @@
+package srvconn
+
+import (
+	"os/user"
+	"strconv"
+	"syscall"
+
+	"github.com/jumpserver/koko/pkg/localcommand"
+)
+
+func BuildNobodyWithOpts(opts ...localcommand.Option) (nobodyOpts []localcommand.Option, err error) {
+	nobody, err := user.Lookup("nobody")
+	if err != nil {
+		return nil, err
+	}
+	uid, _ := strconv.Atoi(nobody.Uid)
+	gid, _ := strconv.Atoi(nobody.Gid)
+	nobodyOpts = make([]localcommand.Option, 0, len(opts)+1)
+	nobodyOpts = append(nobodyOpts, opts...)
+	nobodyCredential := localcommand.WithCmdCredential(&syscall.Credential{Uid: uint32(uid), Gid: uint32(gid)})
+	nobodyOpts = append(nobodyOpts, nobodyCredential)
+	return nobodyOpts, nil
+}
