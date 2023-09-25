3942b63c037d371eb88e7db61d309e2cfa16629f
Eric <xplzv@126.com>
2023-09-25 23:54:18 +0800

    fix: 修复部分执行权限

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,12 +7,12 @@ do
     sleep 2
 done
 # 限制所有可执行目录的权限
-chmod -R  700 /usr/local/sbin/ && chmod -R 700 /usr/local/bin/
-chmod -R  700 /usr/sbin/ && chmod -R 700 /sbin/ && chmod -R 700 /bin/
+chmod -R  700 /usr/local/sbin/* && chmod -R 700 /usr/local/bin/*
+chmod -R  700 /usr/sbin/* && chmod -R 700 /sbin/* && chmod -R 700 /bin/*
 
 
 # 放开部分需要的可执行权限
-chmod 755 `which mysql` `which psql` `which mongosh` `which tsql` `which redis` `which clickhouse`
+chmod 755 `which mysql` `which psql` `which mongosh` `which tsql` `which redis` `which clickhouse-client`
 chmod 755 `which kubectl` `which rawkubectl` `which helm` `which rawhelm`
 
 cd /opt/koko
