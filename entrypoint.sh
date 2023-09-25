6febb0dadaf4d57b172f77aeb3691db03c01dd0a
Eric <xplzv@126.com>
2023-09-25 21:34:51 +0800

    fix: 限制 nobody 的执行权限

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,14 @@ do
     echo "wait for jms_core $CORE_HOST ready"
     sleep 2
 done
+# 限制所有可执行目录的权限
+chmod 700 /usr/local/sbin && chmod 700 /usr/local/bin
+chmod 700 /usr/sbin && chmod 700 /sbin &&  chmod 700 /bin
+
+
+# 放开部分需要的可执行权限
+chmod 755 `which mysql` `which psql` `which mongosh` `which tsql` `which redis` `which clickhouse`
+chmod 755 `which kubectl` `which rawkubectl` `which helm` `which rawhelm`
 
 cd /opt/koko
 ./koko
