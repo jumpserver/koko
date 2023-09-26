14d34891c0032b90613d6f570b8d1b5f127a598b
Eric <xplzv@126.com>
2023-09-26 12:17:50 +0800

    perf: 放开部分命令

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,14 +7,14 @@ do
     sleep 2
 done
 # 限制所有可执行目录的权限
-chmod -R  700 /usr/local/sbin/* && chmod -R 700 /usr/local/bin/*
+chmod -R  700 /usr/local/sbin/* && chmod -R 700 /usr/local/bin/* && chmod -R 700 /usr/bin/*
 chmod -R  700 /usr/sbin/* && chmod -R 700 /sbin/* && chmod -R 700 /bin/*
 
 
 # 放开部分需要的可执行权限
 chmod 755 `which mysql` `which psql` `which mongosh` `which tsql` `which redis` `which clickhouse-client`
 chmod 755 `which kubectl` `which rawkubectl` `which helm` `which rawhelm`
-chmod 755 `which jq` `which less` `which vim`
+chmod 755 `which jq` `which less` `which vim` `which ls` `which bash`
 
 cd /opt/koko
 ./koko
