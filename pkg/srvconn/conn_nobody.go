4f9018ad29ab85e854c5db4502f00639f5bfbb95
Eric <xplzv@126.com>
2023-10-07 15:17:34 +0800

    perf: 重置命令执行的环境变量

--- pkg/srvconn/conn_nobody.go
+++ pkg/srvconn/conn_nobody.go
@@ -18,6 +18,7 @@ func BuildNobodyWithOpts(opts ...localcommand.Option) (nobodyOpts []localcommand
 	nobodyOpts = make([]localcommand.Option, 0, len(opts)+1)
 	nobodyOpts = append(nobodyOpts, opts...)
 	nobodyCredential := localcommand.WithCmdCredential(&syscall.Credential{Uid: uint32(uid), Gid: uint32(gid)})
+	nobodyOpts = append(nobodyOpts, localcommand.WithEnv(make([]string, 0)))
 	nobodyOpts = append(nobodyOpts, nobodyCredential)
 	return nobodyOpts, nil
 }
