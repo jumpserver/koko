768490058fa0a3923fe02f28a5b6c42619399bf9
Eric <xplzv@126.com>
2024-05-28 12:25:47 +0800

    perf: 优化 random

--- pkg/common/random.go
+++ pkg/common/random.go
@@ -7,11 +7,13 @@ import (
 
 const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+var localRand = rand.New(rand.NewSource(time.Now().UnixNano()))
+
 func RandomStr(length int) string {
-	rand.Seed(time.Now().UnixNano())
+	localRand.Seed(time.Now().UnixNano())
 	b := make([]byte, length)
 	for i := range b {
-		b[i] = letters[rand.Intn(len(letters))]
+		b[i] = letters[localRand.Intn(len(letters))]
 	}
 	return string(b)
 }
