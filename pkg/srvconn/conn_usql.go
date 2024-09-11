ac5cd44acef3ffaca7e819839c815cd444285192
Aaron3S <chenyang@fit2cloud.com>
2024-09-11 18:44:11 +0800

    feat: 支持 pg 和 mysql数据库命令行方式的 ssl 连接

--- pkg/srvconn/conn_usql.go
+++ pkg/srvconn/conn_usql.go
@@ -4,6 +4,7 @@ import (
 	"net"
 	"net/url"
 	"strconv"
+	"time"
 
 	"github.com/jumpserver/koko/pkg/localcommand"
 )
@@ -49,7 +50,10 @@ func (conn *USQLConn) Close() error {
 }
 
 func startUSQLCommand(opt *sqlOption) (*localcommand.LocalCommand, error) {
-	args := opt.USQLCommandArgs()
+	args, err := opt.USQLCommandArgs(opt)
+	if err != nil {
+		return nil, err
+	}
 	lcmd, err := localcommand.New("usql", args, localcommand.WithEnv([]string{
 		"PAGE=",
 	}))
@@ -59,13 +63,54 @@ func startUSQLCommand(opt *sqlOption) (*localcommand.LocalCommand, error) {
 	return lcmd, nil
 }
 
-func (o *sqlOption) USQLCommandArgs() []string {
+func (o *sqlOption) USQLCommandArgs(opt *sqlOption) ([]string, error) {
 	var dsnURL url.URL
 	dsnURL.Scheme = o.Schema
 	dsnURL.Host = net.JoinHostPort(o.Host, strconv.Itoa(o.Port))
 	dsnURL.User = url.UserPassword(o.Username, o.Password)
 	dsnURL.Path = o.DBName
+
+	if opt.UseSSL {
+		clientCertKeyPath, err := StoreCAFileToLocal(opt.CertKey)
+		if err != nil {
+			return nil, err
+		}
+		clientCertPath, err := StoreCAFileToLocal(opt.ClientCert)
+		if err != nil {
+			return nil, err
+		}
+		caCertPath, err := StoreCAFileToLocal(opt.CaCert)
+		if err != nil {
+			return nil, err
+		}
+
+		defer ClearTempFileDelay(time.Minute, clientCertPath, clientCertKeyPath, caCertPath)
+
+		params := url.Values{}
+
+		switch o.Schema {
+		case "postgres":
+
+			params.Set("sslcert", clientCertPath)
+			params.Set("sslkey", clientCertKeyPath)
+
+			if opt.CaCert != "" {
+				params.Set("sslrootcert", caCertPath)
+				params.Set("sslmode", "verify-full")
+			} else {
+				params.Set("sslmode", "require")
+			}
+		case "mysql":
+			params.Set("tls", "custom")
+			params.Set("ssl-ca", caCertPath)
+			params.Set("ssl-cert", clientCertPath)
+			params.Set("ssl-key", clientCertKeyPath)
+		}
+		dsnURL.RawQuery = params.Encode()
+	}
+
 	dsn := dsnURL.String()
 	prompt1 := "--variable=PROMPT1=" + o.AssetName + "%R%#"
-	return []string{dsn, prompt1}
+
+	return []string{dsn, prompt1}, nil
 }
