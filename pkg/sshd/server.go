5d417e91b01b78a133b228023d22abbe24b5672f
Eric <xplzv@126.com>
2024-11-11 13:11:48 +0800

    perf: add all supported sshd macs and kexAlgos

--- pkg/sshd/server.go
+++ pkg/sshd/server.go
@@ -25,12 +25,20 @@ const (
 
 var (
 	supportedMACs = []string{"hmac-sha2-256-etm@openssh.com",
-		"hmac-sha2-256", "hmac-sha1"}
+		"hmac-sha2-256", "hmac-sha1", "hmac-sha2-512", "hmac-sha1", "hmac-sha1-96"}
 
+	// "hmac-sha2-256-etm@openssh.com",
+	//"hmac-sha2-512-etm@openssh.com",
+	//"hmac-sha2-256", "hmac-sha2-512",
+	//"hmac-sha1", "hmac-sha1-96",
 	supportedKexAlgos = []string{
 		"curve25519-sha256", "curve25519-sha256@libssh.org",
 		"ecdh-sha2-nistp256", "ecdh-sha2-nistp384", "ecdh-sha2-nistp521",
+		"diffie-hellman-group16-sha512", "diffie-hellman-group14-sha256",
+		"diffie-hellman-group14-sha1", "diffie-hellman-group1-sha1",
 	}
+	// diffie-hellman-group16-sha512  diffie-hellman-group14-sha256
+	//diffie-hellman-group14-sha1 diffie-hellman-group1-sha1
 )
 
 type Server struct {
