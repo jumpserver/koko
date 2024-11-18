80240a69d1cd612fc273fe93652c713ca11aaa49
Eric <xplzv@126.com>
2024-11-18 15:25:17 +0800

    perf: filter unsupported MFA type Face

--- pkg/auth/user_auth.go
+++ pkg/auth/user_auth.go
@@ -36,6 +36,9 @@ func (u *UserAuthClient) Authenticate(ctx context.Context) (user model.User, aut
 		logger.Errorf("User %s Authenticate err: %s", u.Opts.Username, err)
 		return
 	}
+	unsupportedMfaTypes := map[string]bool{
+		"FACE": true,
+	}
 	if resp.Err != "" {
 		switch resp.Err {
 		case ErrLoginConfirmWait:
@@ -44,6 +47,12 @@ func (u *UserAuthClient) Authenticate(ctx context.Context) (user model.User, aut
 		case ErrMFARequired:
 			u.mfaTypes = nil
 			for _, choiceType := range resp.Data.Choices {
+				if _, ok := unsupportedMfaTypes[choiceType]; ok {
+					logger.Infof("User %s login need MFA, skip %s as it not supported", u.Opts.Username,
+						choiceType)
+					continue
+				}
+
 				u.authOptions[choiceType] = authOptions{
 					MFAType: choiceType,
 					Url:     resp.Data.Url,
