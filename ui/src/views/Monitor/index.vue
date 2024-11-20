22b26f8c58fe9d0ce235950ebe070a3f915f5fcc
zhaojisen <1301338853@qq.com>
2024-11-20 13:13:47 +0800

    fixed: Fixed an issue with the Share page Sz command writing buffer

--- ui/src/views/Monitor/index.vue
+++ ui/src/views/Monitor/index.vue
@@ -4,14 +4,9 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import { useTerminalStore } from '@/store/modules/terminal.ts';
 import CustomTerminal from '@/components/CustomTerminal/index.vue';
 
-const terminalStore = useTerminalStore();
-
 const terminalType = ref<string>('common');
-
-terminalStore.setTerminalConfig('enableZmodem', false);
 </script>
 
 <style lang="scss" scoped>
