865735d4f982b1f8d70bdb55ea249ca4d050837c
Eric <xplzv@126.com>
2024-12-18 16:29:56 +0800

    perf: add not security KexAlgos and MACs

--- pkg/sshd/server.go
+++ pkg/sshd/server.go
@@ -25,11 +25,14 @@ const (
 
 var (
 	supportedMACs = []string{"hmac-sha2-256-etm@openssh.com",
-		"hmac-sha2-256", "hmac-sha1"}
+		"hmac-sha2-512-etm@openssh.com",
+		"hmac-sha2-256", "hmac-sha1", "hmac-sha1-96"}
 
 	supportedKexAlgos = []string{
 		"curve25519-sha256", "curve25519-sha256@libssh.org",
 		"ecdh-sha2-nistp256", "ecdh-sha2-nistp384", "ecdh-sha2-nistp521",
+		"diffie-hellman-group14-sha256", "diffie-hellman-group14-sha1",
+		"diffie-hellman-group1-sha1",
 	}
 )
 
