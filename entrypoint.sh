05666dd7639a290eb17579aa0cd6095a0b9b963a
Eric <xplzv@126.com>
2025-01-20 15:53:50 +0800

    perf: use kubectl proxy for k8s session
    perf: add miss file
    
    perf: remove rawkubectl
    
    perf: update k8s proxy
    
    perf: add bash-completion
    
    perf: delete tmp k8s config
    
    perf: update k8s reverse
    
    perf: update port
    
    perf: use unix socket
    
    perf: modify k8s proxy
    
    perf: update log msg
    
    perf: change port

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,4 +16,12 @@ echo "KoKo Version $VERSION, more see https://www.jumpserver.org"
 echo "Quit the server with CONTROL-C."
 echo
 
+# 创建 server.key server.crt
+if [ ! -f /opt/koko/server.key ]; then
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /opt/koko/server.key -out /opt/koko/server.crt -subj "/C=CN/ST=Beijing/L=Beijing/O=JumpServer/OU=JumpServer/CN=JumpServer"
+fi
+
+# /opt/koko to 700 disable other user access
+chmod 700 /opt/koko
+
 exec "$@"
