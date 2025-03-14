26a48c25d6324baa55cddd65094402c19ab31ba3
Eric <xplzv@126.com>
2025-03-14 16:21:43 +0800

    perf: add REDISCLI_RCFILE env

--- pkg/srvconn/conn_nobody.go
+++ pkg/srvconn/conn_nobody.go
@@ -1,11 +1,13 @@
 package srvconn
 
 import (
+	"os"
 	"os/user"
 	"strconv"
 	"syscall"
 
 	"github.com/jumpserver/koko/pkg/localcommand"
+	"github.com/jumpserver/koko/pkg/logger"
 )
 
 func BuildNobodyWithOpts(opts ...localcommand.Option) (nobodyOpts []localcommand.Option, err error) {
@@ -17,8 +19,14 @@ func BuildNobodyWithOpts(opts ...localcommand.Option) (nobodyOpts []localcommand
 	gid, _ := strconv.Atoi(nobody.Gid)
 	nobodyOpts = make([]localcommand.Option, 0, len(opts)+1)
 	nobodyOpts = append(nobodyOpts, opts...)
+	envs := make([]string, 0, 2)
+	redisCliFile := os.Getenv("REDISCLI_RCFILE")
+	if redisCliFile != "" {
+		envs = append(envs, "REDISCLI_RCFILE="+redisCliFile)
+		logger.Infof("rediscli rcfile: %s", redisCliFile)
+	}
 	nobodyCredential := localcommand.WithCmdCredential(&syscall.Credential{Uid: uint32(uid), Gid: uint32(gid)})
-	nobodyOpts = append(nobodyOpts, localcommand.WithEnv(make([]string, 0)))
+	nobodyOpts = append(nobodyOpts, localcommand.WithEnv(envs))
 	nobodyOpts = append(nobodyOpts, nobodyCredential)
 	return nobodyOpts, nil
 }
