083e0d417b58519b02257cdf3aaef8c436d9b8e7
feng <1304903146@qq.com>
2025-05-20 14:32:35 +0800

    fix: FTP downloads have no file records

--- pkg/httpd/sftpvolume.go
+++ pkg/httpd/sftpvolume.go
@@ -236,7 +236,7 @@ func (u *UserVolume) GetFile(path string) (fileData elfinder.FileData, err error
 		return rest, err
 	}
 
-	if err1 := u.recorder.ChunkedRecord(sf.FTPLog, sf, 0, fileInfo.Size()); err1 != nil {
+	if err1 := u.recorder.Record(sf.FTPLog, sf); err1 != nil {
 		logger.Errorf("Record file err: %s", err1)
 	}
 	_, _ = sf.Seek(0, io.SeekStart)
