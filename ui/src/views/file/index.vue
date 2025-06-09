d6de12698468777c50e79db6d9aac15973adf3cd
Eric <xplzv@126.com>
2025-06-09 19:13:49 +0800

    perf: add sftp vue file

---
+++ ui/src/views/file/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="h-full w-full">
+   <FileManager :sftp-token="token" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import FileManager from '@/components/Drawer/components/FileManagement/index.vue';
+
+// 从 url params 中获取 token
+const token = new URLSearchParams(window.location.search).get('token') || '';
+
+</script>
