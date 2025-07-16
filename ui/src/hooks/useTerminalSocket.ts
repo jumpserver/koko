2315bbeb520d770cb2d05975adc25342aab222e5
zhaojisen <1301338853@qq.com>
2025-07-16 12:58:35 +0800

    Fixed: session share

--- ui/src/hooks/useTerminalSocket.ts
+++ ui/src/hooks/useTerminalSocket.ts
@@ -94,8 +94,7 @@ export const useTerminalSocket = () => {
   }));
 
   const autoTerminalFit = watch([width, height], ([_newWidth, _newHeight]: [number, number]) => {
-    if (!terminalRef.value || !fitAddon)
-      return;
+    if (!terminalRef.value || !fitAddon) return;
 
     nextTick(() => {
       fitAddon.fit();
@@ -107,8 +106,7 @@ export const useTerminalSocket = () => {
   });
 
   const debouncedResize = useDebounceFn(({ cols, rows }) => {
-    if (!fitAddon || !socketRef.value)
-      return;
+    if (!fitAddon || !socketRef.value) return;
 
     fitAddon.fit();
 
@@ -130,8 +128,7 @@ export const useTerminalSocket = () => {
    * @description 分发 Socket 消息
    */
   const dispatch = (socketData: string) => {
-    if (!socketData || !socketRef.value || !terminalRef.value)
-      return;
+    if (!socketData || !socketRef.value || !terminalRef.value) return;
 
     const parsedMessageData = JSON.parse(socketData);
 
@@ -173,14 +170,17 @@ export const useTerminalSocket = () => {
         const info = JSON.parse(parsedMessageData.data);
 
         featureSetting.value = info.setting;
-        connectionStore.setConnectionState({
-          assetName: info.asset.name,
-        });
+
+        if (info.asset.name) {
+          connectionStore.setConnectionState({
+            assetName: info.asset.name,
+          });
+        }
 
         updateIcon(info.setting);
 
         socketRef.value!.send(
-          formatMessage(terminalId.value, FORMATTER_MESSAGE_TYPE.TERMINAL_INIT, JSON.stringify(terminalData)),
+          formatMessage(terminalId.value, FORMATTER_MESSAGE_TYPE.TERMINAL_INIT, JSON.stringify(terminalData))
         );
 
         break;
@@ -367,15 +367,13 @@ export const useTerminalSocket = () => {
     if (zmodemTransferStatus.value) {
       try {
         sentry.consume(socketMessage.data);
-      }
-      catch (_e) {
+      } catch (_e) {
         if (sentry.get_confirmed_session()) {
           sentry.get_confirmed_session()?.abort();
           message.error('File transfer error, file transfer interrupted');
         }
       }
-    }
-    else {
+    } else {
       writeBufferToTerminal(true, false, terminalRef.value, socketMessage.data);
     }
   };
@@ -391,8 +389,7 @@ export const useTerminalSocket = () => {
     sentry = createSentry(terminalRef.value!, socketRef.value!, lastSendTime);
 
     socketRef.value.onopen = () => {
-      if (pingInterval.value)
-        clearInterval(pingInterval.value);
+      if (pingInterval.value) clearInterval(pingInterval.value);
 
       pingInterval.value = setInterval(() => {
         if (isSocketClosing(socketRef.value!)) {
@@ -415,8 +412,7 @@ export const useTerminalSocket = () => {
       }, 25 * 1000);
     };
     socketRef.value.onclose = () => {
-      if (!terminalRef.value)
-        return;
+      if (!terminalRef.value) return;
 
       terminalRef.value.write(`\r\n`);
       terminalRef.value.write(`\x1B[31m${t('WebSocketClosed')}\x1B[0m`);
@@ -426,8 +422,7 @@ export const useTerminalSocket = () => {
 
       if (typeof message.data === 'object') {
         handleBinaryMessage(message);
-      }
-      else {
+      } else {
         dispatch(message.data);
       }
     };
@@ -451,8 +446,7 @@ export const useTerminalSocket = () => {
     containerRef.value!.addEventListener('contextmenu', async (e: MouseEvent) => {
       // 只有在开启右键快速复制时才允许粘贴
       // TODO 对于 terminal 的 contextmenu 后续需要进行封装
-      if (e.ctrlKey || terminalSettingsStore.quickPaste !== '1')
-        return;
+      if (e.ctrlKey || terminalSettingsStore.quickPaste !== '1') return;
 
       e.preventDefault();
 
@@ -460,8 +454,7 @@ export const useTerminalSocket = () => {
 
       try {
         text = await readText();
-      }
-      catch (_e) {
+      } catch (_e) {
         if (selectionText.value) {
           text = selectionText.value;
         }
@@ -589,8 +582,7 @@ export const useTerminalSocket = () => {
   };
 
   onMounted(() => {
-    if (!containerRef.value)
-      return;
+    if (!containerRef.value) return;
 
     createTerminal();
     createWebSocket();
