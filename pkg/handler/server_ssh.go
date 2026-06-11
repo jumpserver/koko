f0d3474a265beee77fe91b4e4f11255b89d431b2
Zhaoji Wang <hwoam@outlook.com>
2026-06-11 13:00:25 -0700

    fix: preserve SFTP subsystem exit status

--- pkg/handler/server_ssh.go
+++ pkg/handler/server_ssh.go
@@ -98,13 +98,18 @@ func (s *Server) SFTPHandler(sess ssh.Session) {
 	reqID := common.UUID()
 	logger.Infof("SFTP request %s: Handler start", reqID)
 	req := sftp.NewRequestServer(sess, handlers)
-	if err := req.Serve(); err == io.EOF {
+	serveErr := req.Serve()
+	if errors.Is(serveErr, io.EOF) {
 		logger.Debugf("SFTP request %s: Exited session.", reqID)
-	} else if err != nil {
-		logger.Errorf("SFTP request %s: Server completed with error %s", reqID, err)
+	} else if serveErr != nil {
+		logger.Errorf("SFTP request %s: Server completed with error %s", reqID, serveErr)
 	}
-	_ = req.Close()
 	sftpHandler.Close()
+	if serveErr != nil && !errors.Is(serveErr, io.EOF) {
+		if err := sess.Exit(1); err != nil {
+			logger.Errorf("SFTP request %s: Send exit status failed: %s", reqID, err)
+		}
+	}
 	logger.Infof("SFTP request %s: Handler exit.", reqID)
 }
 
